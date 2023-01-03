7b42cb4f77155581be5c230ffaa0c3ae8cfa84e4
Michelle McCann <105986556+Michelle-io@users.noreply.github.com>
2023-01-03 06:48:33 +0000

    added words list for game

---
+++ word_list.py
@@ -0,0 +1,9 @@
+words = 'abruptly absurd abyss affix askew avenue awkward axiom azure ' \
+        'bagpipes bandwagon banjo bayou beekeeper bikini' \
+        'blitz blizzard boggle bookworm boxcar boxful buckaroo buffalo ' \
+        'buffoon buxom buzzard fishhook fixable fjord flapjack flopping ' \
+        'fluffiness hyphen iatrogenic icebox injury ivory ivy jackpot ' \
+        'kiwifruit klutz knapsack larynx lengths lucky luxury lymph marquis ' \
+        'matrix puzzling quartz queue quips quixotic quiz unzip uptown ' \
+        'vaporize vixen vodka voodoo vortex voyeurism walkway waltz wave' \
+        'wavy waxy wellspring'.split()
