a5db30c63cd15e0aed96167c3feae3f54f4cb1b3
Michelle McCann <105986556+Michelle-io@users.noreply.github.com>
2023-01-03 06:51:38 +0000

    add function to return players name

--- run.py
+++ run.py
@@ -7,3 +7,11 @@ def get_random_word():
     """ Return a random word from the list of words"""
     game_word = random.choice(words)
     return game_word
+
+
+def player_name():
+    """ Ask player for their name"""
+    user_name = input('What is your name? ')
+    return user_name
+
+
